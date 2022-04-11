17b2ce0d12667db2086a3c05a72264e7f95a2a60
urbanizzz <yuraoskin@gmail.com>
2022-04-12 00:05:34 +0700

    Implementing of config
    Reading config from yaml file and pack it to datatypes

--- src/Config.hs
+++ src/Config.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | A module to provide a configuration reader for other modules.
 module Config
   ( getBotConfig,
@@ -6,17 +8,49 @@ module Config
   )
 where
 
-import qualified ConfigurationTypes
+import qualified ConfigurationTypes as CT
 import qualified EchoBot
 import qualified Logger.Impl
+import Logger (Level (..))
+import qualified System.IO
 
 -- | Gets the bot config. In any case it can provide reasonable
 -- default values.
 getBotConfig :: IO EchoBot.Config
-getBotConfig = error "Not implemented"
+getBotConfig = do
+  (CT.Config _ _ bot) <- CT.configFromYaml
+  pure EchoBot.Config 
+    { EchoBot.confHelpReply = CT.confHelpReply bot
+    , EchoBot.confRepeatReply = CT.confRepeatReply bot
+    , EchoBot.confRepetitionCount = CT.confRepeatDefault bot
+    }
 
 getLoggerConfig :: IO Logger.Impl.Config
-getLoggerConfig = error "Not implemented"
+getLoggerConfig = do
+  (CT.Config logger _ _) <- CT.configFromYaml
+  logHandler <- getLogHandler $ CT.confPath logger
+  let minLevel = getMinLevel $ CT.confLevel logger
+  pure Logger.Impl.Config
+    { Logger.Impl.confFileHandle  = logHandler
+    , Logger.Impl.confMinLevel    = minLevel
+    }
 
-getFrontEndType :: IO ConfigurationTypes.FrontEndType
-getFrontEndType = error "Not implemented"
+getLogHandler :: System.IO.FilePath -> IO System.IO.Handle
+getLogHandler "stderr" = pure System.IO.stderr
+getLogHandler "stdout" = pure System.IO.stdout
+getLogHandler file = do
+  h <- System.IO.openFile file System.IO.AppendMode
+  System.IO.hSetBuffering h System.IO.LineBuffering
+  pure h
+
+getMinLevel :: CT.LogLevel -> Level
+getMinLevel CT.Debug    = Debug
+getMinLevel CT.Info     = Info
+getMinLevel CT.Warning  = Warning
+getMinLevel CT.Error    = Error
+
+getFrontEndType :: IO CT.FrontEndType
+getFrontEndType = do
+  (CT.Config _ front _) <- CT.configFromYaml
+  pure . CT.confFrontEndType $ front
+
--- src/ConfigurationTypes.hs
+++ src/ConfigurationTypes.hs
@@ -1,8 +1,133 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ConfigurationTypes
-  ( FrontEndType (..),
+  ( FrontEndTypeConfig (..)
+  , FrontEndType (..)
+  , LoggerConfig (..)
+  , LogLevel (..)
+  , BotConfig (..)
+  , Config (..)
+  , configFromYaml
   )
 where
+
+import Data.Yaml (FromJSON (..), decodeFileEither, withObject, withText, (.:?), (.!=))
+import Data.Text (Text)
+import qualified System.IO
+
+configFile :: System.IO.FilePath
+configFile = "./config.yaml"
+
+defaultFrontEnd :: FrontEndType
+defaultFrontEnd = ConsoleFrontEnd
+
+defaultFrontEndConfig :: FrontEndTypeConfig
+defaultFrontEndConfig = FrontEndTypeConfig defaultFrontEnd
+
+defaultRepeatCount, maxRepeatCount :: Int
+defaultRepeatCount = 3
+maxRepeatCount = 5
+
+defaultRepeatReply, defaultHelpReply :: Text
+defaultRepeatReply = "Current count of repetitions is {count}. Enter number for new count (1-5)." :: Text
+defaultHelpReply = "Echobot - simple echo bot.\n/help to get this help\n/repeat to set the number of repetitions" :: Text
+
+defaultBotConfig :: BotConfig
+defaultBotConfig = BotConfig defaultRepeatCount defaultRepeatReply defaultHelpReply
+
+defaultLogHandler :: System.IO.FilePath
+defaultLogHandler = "./echo-bot.log" :: System.IO.FilePath
+
+defaultLevel :: LogLevel
+defaultLevel = Warning
+
+defaultLoggerConfig :: LoggerConfig
+defaultLoggerConfig = LoggerConfig defaultLogHandler defaultLevel 
+
+defaultConfig :: Config
+defaultConfig = Config defaultLoggerConfig defaultFrontEndConfig defaultBotConfig
 
 data FrontEndType
   = ConsoleFrontEnd
   | TelegramFrontEnd
+  deriving Show
+
+instance FromJSON FrontEndType where
+  parseJSON = withText "FromJSON FrontEndType" $ \t ->
+    case t of
+      "console"   -> pure ConsoleFrontEnd
+      "telegram"  -> pure TelegramFrontEnd
+      _           -> pure defaultFrontEnd
+
+newtype FrontEndTypeConfig = FrontEndTypeConfig
+  { confFrontEndType  :: FrontEndType
+  }
+
+instance FromJSON FrontEndTypeConfig where
+  parseJSON = withObject "FromJSON FrontEndTypeConfig" $ \o -> FrontEndTypeConfig
+    <$> o .:? "frontend" .!= defaultFrontEnd
+    
+data LoggerConfig = LoggerConfig
+  { confPath  :: FilePath
+  , confLevel :: LogLevel
+  }
+
+instance FromJSON LoggerConfig where
+  parseJSON = withObject "FromJSON LoggerConfig" $ \o -> LoggerConfig
+    <$> o .:? "path"      .!= defaultLogHandler
+    <*> o .:? "logLevel"  .!= defaultLevel
+
+data LogLevel
+  = Debug
+  | Info
+  | Warning
+  | Error
+
+instance FromJSON LogLevel where
+  parseJSON = withText "FromJSON LogLevel" $ \t ->
+    case t of
+      "debug"   -> pure Debug
+      "info"    -> pure Info
+      "warning" -> pure Warning
+      "error"   -> pure Error
+      _         -> pure defaultLevel
+
+data BotConfig = BotConfig
+  { confRepeatDefault :: Int
+  , confRepeatReply   :: Text
+  , confHelpReply     :: Text
+  }
+
+instance FromJSON BotConfig where
+  parseJSON = withObject "FromJSON BotConfig" $ \o -> do
+    rawCount    <- o .:? "repeatDefault" .!= defaultRepeatCount
+    repeatReply <- o .:? "repeatReply"   .!= defaultRepeatReply
+    helpReply   <- o .:? "helpReply"     .!= defaultHelpReply
+    let readyCount = restrictCount rawCount
+    pure (BotConfig readyCount repeatReply helpReply)
+
+restrictCount :: Int -> Int
+restrictCount x
+  | x>0 && x<maxRepeatCount = x
+  | otherwise = defaultRepeatCount
+
+data Config = Config
+  { confLogger      :: LoggerConfig
+  , confBotFrontEnd :: FrontEndTypeConfig
+  , confBot         :: BotConfig
+  }
+
+instance FromJSON Config where
+  parseJSON = withObject "FromJSON Config" $ \o -> Config
+    <$> o .:? "logger"      .!= defaultLoggerConfig
+    <*> o .:? "botFrontEnd" .!= defaultFrontEndConfig
+    <*> o .:? "bot"         .!= defaultBotConfig
+
+configFromYaml :: IO Config
+configFromYaml = do
+  errOrConfig <- decodeFileEither configFile
+  let config = case errOrConfig of
+                (Right x) -> x
+                _         -> defaultConfig
+  pure config
+
