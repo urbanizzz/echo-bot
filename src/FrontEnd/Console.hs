9a5e3c9061c36ac28e30230902e1fb1b006fc263
urbanizzz <yuraoskin@gmail.com>
2022-04-14 13:48:22 +0700

    Implementing console bot

--- src/FrontEnd/Console.hs
+++ src/FrontEnd/Console.hs
@@ -12,15 +12,36 @@ where
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified EchoBot
+import Control.Monad (forever)
+import Text.Read (readMaybe)
+import qualified Data.Map.Strict as Map
 
 newtype Handle = Handle
   { hBotHandle :: EchoBot.Handle IO T.Text
   }
 
+type RepetitionCount = Int
+
 run :: Handle -> IO ()
-run _ = do
+run h = do
   TIO.putStrLn "Welcome to the echo-bot!"
-  -- 1. Read a line from the console.
-  -- 2. Send it to the bot, get its response and output it.
-  -- 3. Go to 1.
-  error "Not implemented"
+  forever $ do
+    msg <- TIO.getLine
+    response <- EchoBot.respond (hBotHandle h) (EchoBot.MessageEvent msg)
+    mapM_ (handleResponse h) response
+
+handleResponse :: Handle -> EchoBot.Response T.Text -> IO ()
+handleResponse _ (EchoBot.MessageResponse x) = TIO.putStrLn x
+handleResponse h (EchoBot.MenuResponse title options) = do
+  TIO.putStrLn title
+  rawText <- TIO.getLine
+  let number = getNumberFromRawText rawText
+  let maybeEvent = Map.lookup number $ Map.fromList options
+  _ <- maybe (pure []) (EchoBot.respond (hBotHandle h)) maybeEvent
+  pure ()
+
+getNumberFromRawText :: T.Text -> RepetitionCount
+getNumberFromRawText text = case (readMaybe . T.unpack $ text) of
+  Just x -> x
+  _      -> 0
+ 
